de0b4a34362224eb80ee1d9e6d9abf6ca6730481
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2024-09-27 16:35:08 +0300

    OK-630: Lisätään dev-CORS-säännöt

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/OvaraBackendApplication.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/OvaraBackendApplication.scala
@@ -4,7 +4,8 @@ import fi.oph.ovara.backend.repository.OvaraDatabase
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.{ApplicationArguments, ApplicationRunner, SpringApplication}
-import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.{Bean, Profile}
+import org.springframework.web.servlet.config.annotation.{CorsRegistry, WebMvcConfigurer}
 
 object OvaraBackendApplication {
     def main(args: Array[String]): Unit = {
@@ -18,8 +19,16 @@ class OvaraBackendApplication {
   val db: OvaraDatabase = null
 
   @Bean
-  def applicationRunner() =
+  def applicationRunner(): ApplicationRunner =
     new ApplicationRunner:
       override def run(args: ApplicationArguments): Unit =
         println("STARTING APPLICATION RUNNER")
+ 
+  @Profile(Array("dev"))
+  @Bean
+  def corsConfigurer(): WebMvcConfigurer = new WebMvcConfigurer {
+    override def addCorsMappings(registry: CorsRegistry): Unit = {
+      registry.addMapping("/**").allowedOrigins("https://localhost:3405")
+    }
+  }
 }
