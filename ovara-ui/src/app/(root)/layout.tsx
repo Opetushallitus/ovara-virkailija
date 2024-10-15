dc09398e5cc4c314731e052297102261ed3ee391
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2024-10-15 09:09:16 +0300

    OK-688: Siirretään Header-komponentin muodostus omaan client komponenttiinsa

--- ovara-ui/src/app/(root)/layout.tsx
+++ ovara-ui/src/app/(root)/layout.tsx
@@ -1,26 +1,16 @@
-'use client';
-
 import Header from '../components/header';
 import { PageLayout } from '../components/page-layout';
-import { useTranslations } from 'next-intl';
-import { usePathname } from 'next/navigation';
+import { PageContent } from '../components/page-content';
 
 export default function HomeLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const t = useTranslations('Header');
-  const currentRoute = usePathname();
-  const isHome = currentRoute == '/';
-  const headerTranslationKey = isHome
-    ? 'home'
-    : currentRoute.match(/[a-z]+-?[a-z]*?/gm)?.join('.');
   return (
-    <PageLayout
-      header={<Header isHome={isHome} title={t(headerTranslationKey)} />}
-    >
-      {children}
+    <PageLayout>
+      <Header />
+      <PageContent>{children}</PageContent>
     </PageLayout>
   );
 }
