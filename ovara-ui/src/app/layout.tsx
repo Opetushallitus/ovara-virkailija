c26c17a3011b788212a81571abba9a83b819eb52
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2024-10-16 14:22:43 +0300

    OK-688: Poistetaan turhaa koodia

--- ovara-ui/src/app/layout.tsx
+++ ovara-ui/src/app/layout.tsx
@@ -6,8 +6,6 @@ import { getLocale, getMessages } from 'next-intl/server';
 import { type OphLanguage } from '@opetushallitus/oph-design-system';
 import Script from 'next/script';
 import { configuration } from './lib/configuration';
-import PermissionProvider from './components/permission-provider';
-import ReactQueryClientProvider from './components/react-query-client-provider';
 
 export const metadata: Metadata = {
   title: 'Opiskelijavalinnan raportointi',
@@ -28,13 +26,9 @@ export default async function RootLayout({
       <body>
         <AppRouterCacheProvider>
           <NextIntlClientProvider messages={messages}>
-            <ReactQueryClientProvider>
-              <PermissionProvider>
-                <OphNextJsThemeProvider variant="oph" lang={locale}>
-                  {children}
-                </OphNextJsThemeProvider>
-              </PermissionProvider>
-            </ReactQueryClientProvider>
+            <OphNextJsThemeProvider variant="oph" lang={locale}>
+              {children}
+            </OphNextJsThemeProvider>
           </NextIntlClientProvider>
         </AppRouterCacheProvider>
       </body>
