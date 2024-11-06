0c563dee354375a4f1afea2cdc8aa741c2d76be5
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2024-11-06 11:50:27 +0200

    OK-688: Lisätään apufunktioita alkamiskausien käsittelyyn ja queryn muodostamiseen

---
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/utils/RepositoryUtils.scala
@@ -0,0 +1,36 @@
+package fi.oph.ovara.backend.utils
+
+import scala.util.matching.Regex
+
+object RepositoryUtils {
+  def extractAlkamisvuosiAndKausi(alkamiskaudet: List[String]): List[(Int, String)] = {
+    val alkamiskausiRegex: Regex = """(\d{4})_([a-z]+)""".r
+    alkamiskaudet.flatMap(alkamiskausi =>
+      for alkamiskausiMatches <- alkamiskausiRegex.findAllMatchIn(alkamiskausi) yield
+        val kausi = alkamiskausiMatches.group(2) match
+          case "syksy" => "kausi_s"
+          case "kevat" => "kausi_k"
+
+        (alkamiskausiMatches.group(1).toInt, kausi)
+    )
+  }
+
+  def makeAlkamiskausiQueryStr(tableNames: List[String], alkamiskausi: (Int, String)): String = {
+    s"(${tableNames.map(tableName => s"($tableName.koulutuksen_alkamisvuosi = ${alkamiskausi._1} AND " +
+      s"$tableName.koulutuksen_alkamiskausi_koodiuri LIKE '${alkamiskausi._2}%')").mkString(" OR ")})"
+  }
+
+  def makeAlkamiskaudetQueryStr(operator: String, tableNames: List[String], alkamiskaudet: List[(Int, String)]): String = {
+    def hasMoreThanOne(alkamiskaudet: List[(Int, String)]) = {
+      alkamiskaudet.size > 1
+    }
+
+    if (alkamiskaudet.isEmpty) {
+      ""
+    } else {
+      s"$operator ${if hasMoreThanOne(alkamiskaudet) then "(" else ""}" +
+        s"${alkamiskaudet.map(alkamiskausi => makeAlkamiskausiQueryStr(tableNames = tableNames, alkamiskausi = alkamiskausi)).mkString(" OR ")}" +
+        s"${if hasMoreThanOne(alkamiskaudet) then ")" else ""}"
+    }
+  }
+}
---
+++ ovara-backend/src/test/scala/fi/oph/ovara/backend/utils/RepositoryUtilsSpec.scala
@@ -0,0 +1,62 @@
+package fi.oph.ovara.backend.utils
+
+import org.scalatest.*
+import org.scalatest.flatspec.*
+
+class RepositoryUtilsSpec extends AnyFlatSpec {
+  "extractAlkamisvuosiAndKausi" should "return year and alkamiskausikoodiuri for one alkamiskausi" in {
+    assert(RepositoryUtils.extractAlkamisvuosiAndKausi(List("2023_syksy")) == List((2023, "kausi_s")))
+  }
+
+  it should "return year and alkamiskausikoodiuri for several alkamiskaudet" in {
+    assert(
+      RepositoryUtils.extractAlkamisvuosiAndKausi(
+        List("2023_syksy", "2021_kevat", "not_alkamiskausi", "2020_syksy")
+      ) == List((2023, "kausi_s"), (2021, "kausi_k"), (2020, "kausi_s"))
+    )
+  }
+
+  it should "return empty list when no alkamiskaudet specified" in {
+    assert(
+      RepositoryUtils.extractAlkamisvuosiAndKausi(List()) == List()
+    )
+  }
+
+  "makeAlkamiskausiQueryStr" should "return query string with alkamisvuosi and alkamiskausityyppi in combined query" in {
+    assert(
+      RepositoryUtils.makeAlkamiskausiQueryStr(
+        List("t", "hk"),
+        (2023, "kausi_s")
+      ) == "((t.koulutuksen_alkamisvuosi = 2023 AND t.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_s%') " +
+        "OR (hk.koulutuksen_alkamisvuosi = 2023 AND hk.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_s%'))"
+    )
+  }
+
+  "makeAlkamiskaudetQueryStr" should "return query string with one alkamiskaudet" in {
+    assert(
+      RepositoryUtils.makeAlkamiskaudetQueryStr(
+        "AND",
+        List("t", "hk"),
+        List((2023, "kausi_s"))
+      ) == "AND ((t.koulutuksen_alkamisvuosi = 2023 AND t.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_s%') " +
+        "OR (hk.koulutuksen_alkamisvuosi = 2023 AND hk.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_s%'))"
+    )
+  }
+
+  it should "return query string with several alkamiskaudet" in {
+    assert(
+      RepositoryUtils.makeAlkamiskaudetQueryStr(
+        "AND",
+        List("t", "hk"),
+        List((2023, "kausi_s"), (2021, "kausi_k"))
+      ) == "AND (((t.koulutuksen_alkamisvuosi = 2023 AND t.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_s%') " +
+        "OR (hk.koulutuksen_alkamisvuosi = 2023 AND hk.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_s%')) " +
+        "OR ((t.koulutuksen_alkamisvuosi = 2021 AND t.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_k%') " +
+        "OR (hk.koulutuksen_alkamisvuosi = 2021 AND hk.koulutuksen_alkamiskausi_koodiuri LIKE 'kausi_k%')))"
+    )
+  }
+
+  it should "return empty string when there are no alkamiskaudet specified" in {
+    assert(RepositoryUtils.makeAlkamiskaudetQueryStr("AND", List("t", "hk"), List()) == "")
+  }
+}
