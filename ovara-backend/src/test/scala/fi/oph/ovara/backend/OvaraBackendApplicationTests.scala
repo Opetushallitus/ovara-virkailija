2143044eb7d8d1bfa02ed8bf2b46128e72f91566
Marja Kari <marja.kari@amban.fi>
2025-01-16 16:02:08 +0200

    OK-790 simppeli api-testi autentikoinnille

--- ovara-backend/src/test/scala/fi/oph/ovara/backend/OvaraBackendApplicationTests.scala
+++ ovara-backend/src/test/scala/fi/oph/ovara/backend/OvaraBackendApplicationTests.scala
@@ -5,23 +5,41 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.http.MediaType
+import org.springframework.security.test.context.support.WithMockUser
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.{content, status}
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles(Array("test"))
 class OvaraBackendApplicationTests {
     @Autowired
     private val mvc: MockMvc = null
 
     @Test
     @throws[Exception]
-    def getResponseFromHealthcheck(): Unit = {
-        mvc.perform(MockMvcRequestBuilders.get("/api/ping").accept(MediaType.APPLICATION_JSON))
+    def get200ResponseFromHealthcheckUnautheticated(): Unit = {
+        mvc.perform(MockMvcRequestBuilders.get("/api/healthcheck").accept(MediaType.APPLICATION_JSON))
           .andExpect(status.isOk)
           .andExpect(content.string(equalTo("Ovara application is running!")))
+    }
+
+    @Test
+    @throws[Exception]
+    def get401ResponseFromAuthenticatedApi(): Unit = {
+        mvc.perform(MockMvcRequestBuilders.get("/api/alkamisvuodet").accept(MediaType.APPLICATION_JSON))
+          .andExpect(status.isUnauthorized)
+    }
+
+    @Test
+    @WithMockUser(username = "testuser", roles = Array("USER"))
+    def getAuthenticatedUserGets200ResponseFromAuthenticatedApi(): Unit = {
+        mvc.perform(MockMvcRequestBuilders.get("/api/alkamisvuodet"))
+          .andExpect(status().isOk)
+          .andExpect(content().string("[ \"2024\", \"2023\", \"2022\" ]"))
     }
 
 }
