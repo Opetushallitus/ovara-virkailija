7b08d791504a1e026d07021286f46f7d63d937cb
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2025-01-16 08:08:28 +0200

    OK-754: Poistetaan turhaksi jäänyt OrganisaatioPerOrganisaatiotyyppi case class

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/domain/Organisaatio.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/domain/Organisaatio.scala
@@ -2,12 +2,6 @@ package fi.oph.ovara.backend.domain
 
 case class Organisaatio(organisaatio_oid: String, organisaatio_nimi: Kielistetty, organisaatiotyypit: List[String])
 
-case class OrganisaatioPerOrganisaatiotyyppi(
-    organisaatio_oid: String,
-    organisaatio_nimi: Kielistetty,
-    organisaatiotyyppi: String
-)
-
 case class OrganisaatioParentChild(parent_oid: String, child_oid: String, organisaatio: Organisaatio)
 
 case class OrganisaatioHierarkia(
