79531ed880388d858ad537b176ae4cdb28f364f6
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2025-01-20 14:15:05 +0200

    OK-690: Eriytetään excelin lähettäminen omaksi apufunktiokseen

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/raportointi/Controller.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/raportointi/Controller.scala
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import fi.oph.ovara.backend.domain.UserResponse
 import fi.oph.ovara.backend.service.{CommonService, KoulutuksetToteutuksetHakukohteetService, UserService}
 import jakarta.servlet.http.HttpServletResponse
+import org.apache.poi.ss.usermodel.Workbook
 import org.slf4j.{Logger, LoggerFactory}
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
@@ -67,6 +68,27 @@ class Controller(
 
   // RAPORTIT
 
+  def sendExcel(wb: Workbook, response: HttpServletResponse): Unit = {
+    try {
+      LOG.info(s"Sending excel in the response")
+      val date: LocalDateTime = LocalDateTime.now().withNano(0)
+      val dateTimeStr         = date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+      val out                 = response.getOutputStream
+      response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+      response.setHeader(
+        HttpHeaders.CONTENT_DISPOSITION,
+        s"attachment; filename=\"koulutukset-toteutukset-hakukohteet-$dateTimeStr.xlsx\""
+      )
+      wb.write(out)
+      out.close()
+      wb.close()
+    } catch {
+      case e: Exception =>
+        LOG.error(s"Error sending excel: ${e.getMessage}")
+        response.sendError(500)
+    }
+  }
+
   @GetMapping(path = Array("koulutukset-toteutukset-hakukohteet"))
   def koulutukset_toteutukset_hakukohteet(
       @RequestParam("alkamiskausi") alkamiskausi: java.util.Collection[String],
@@ -101,23 +123,8 @@ class Controller(
       maybeHakukohteenTila,
       maybeValintakoe
     )
-    try {
-      LOG.info(s"Sending excel in the response")
-      val date: LocalDateTime = LocalDateTime.now().withNano(0)
-      val dateTimeStr         = date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
-      val out                 = response.getOutputStream
-      response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-      response.setHeader(
-        HttpHeaders.CONTENT_DISPOSITION,
-        s"attachment; filename=\"koulutukset-toteutukset-hakukohteet-$dateTimeStr.xlsx\""
-      )
-      wb.write(out)
-      out.close()
-      wb.close()
-    } catch {
-      case e: Exception =>
-        LOG.error(s"Error sending excel: ${e.getMessage}")
-        response.sendError(500)
-    }
+
+    sendExcel(wb, response)
   }
+
 }
