03387609b9519177cc563ab6ba2aa2729433ce84
Marja Kari <marja.kari@amban.fi>
2025-01-30 17:38:27 +0200

    OK-804 korjattu auditlog-konffiin lokittava class

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/security/AuthenticationEventListener.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/security/AuthenticationEventListener.scala
@@ -5,12 +5,8 @@ import fi.oph.ovara.backend.utils.AuditLog.{audit, getUser}
 import fi.oph.ovara.backend.utils.AuditOperation.Login
 import fi.vm.sade.auditlog.{Changes, Target}
 import jakarta.servlet.http.HttpServletRequest
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent
-import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.{RequestContextHolder, ServletRequestAttributes}
 
