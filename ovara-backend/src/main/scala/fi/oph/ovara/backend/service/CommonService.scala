5ad5963fbaf8ae43ae9c4789e8496cb4c0cd9f22
Heidi Lammi-Mihaljov <heidi.lammi-mihaljov@gofore.com>
2025-02-19 11:12:48 +0200

    OK-692: Poistetaan default hauntyyppi-parametri selectDistinctExistingHaut-funktiosta

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/service/CommonService.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/service/CommonService.scala
@@ -1,6 +1,6 @@
 package fi.oph.ovara.backend.service
 
-import fi.oph.ovara.backend.domain.{Haku, Hakukohde, Koodi, Organisaatio, OrganisaatioHierarkia}
+import fi.oph.ovara.backend.domain.*
 import fi.oph.ovara.backend.repository.{CommonRepository, OvaraDatabase}
 import fi.oph.ovara.backend.utils.Constants.{
   KOULUTUSTOIMIJARAPORTTI,
@@ -37,7 +37,7 @@ class CommonService(commonRepository: CommonRepository, userService: UserService
       db.run(
         commonRepository
           .selectDistinctExistingHakukohteetWithSelectedOrgsAsJarjestaja(allowedOrgOidsFromSelection, haut),
-        "selectDistinctExistingHakukohteet"
+        "selectDistinctExistingHakukohteetWithSelectedOrgsAsJarjestaja"
       )
     } else {
       Vector()
