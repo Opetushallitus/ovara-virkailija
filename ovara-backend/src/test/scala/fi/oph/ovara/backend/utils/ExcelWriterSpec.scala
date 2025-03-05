dc258fa31e50c1defb2f3aa60ae7cd8c31151f53
Marja Kari <marja.kari@amban.fi>
2025-03-05 10:20:47 +0200

    OK-689 testifiksi

--- ovara-backend/src/test/scala/fi/oph/ovara/backend/utils/ExcelWriterSpec.scala
+++ ovara-backend/src/test/scala/fi/oph/ovara/backend/utils/ExcelWriterSpec.scala
@@ -28,7 +28,8 @@ class ExcelWriterSpec extends AnyFlatSpec {
     "raportti.kokonaispisteet"               -> "Kokonaispisteet SV",
     "raportti.hylkTaiPerSyy"                 -> "Hylkäämisen tai peruuntumisen syy SV",
     "raportti.toimipiste"                    -> "Toimipiste SV",
-    "raportti.otsikko"                       -> "Otsikko SV",
+    "raportti.hakukohde"                     -> "Hakukohde SV",
+    "raportti.oppilaitos"                    -> "Oppilaitos SV",
     "raportti.hakijat-yht"                   -> "Hakijat SV",
     "raportti.ensisijaisia"                  -> "Ensisijaisia SV",
     "raportti.varasija"                      -> "Varasija SV",
@@ -1550,7 +1551,7 @@ class ExcelWriterSpec extends AnyFlatSpec {
     assert(wb.getSheetAt(0).getRow(0).getCell(2).getStringCellValue == "Kansalaisuus SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(3).getStringCellValue == "raportti.oppijanumero")
     assert(wb.getSheetAt(0).getRow(0).getCell(4).getStringCellValue == "raportti.hakemusOid")
-    assert(wb.getSheetAt(0).getRow(0).getCell(5).getStringCellValue == "raportti.oppilaitos")
+    assert(wb.getSheetAt(0).getRow(0).getCell(5).getStringCellValue == "Oppilaitos SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(6).getStringCellValue == "Toimipiste SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(7).getStringCellValue == "Hakukohde SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(8).getStringCellValue == "raportti.hakukohdeOid")
@@ -1595,7 +1596,7 @@ class ExcelWriterSpec extends AnyFlatSpec {
     assert(wb.getSheetAt(0).getRow(0).getCell(2).getStringCellValue == "Kansalaisuus SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(3).getStringCellValue == "raportti.oppijanumero")
     assert(wb.getSheetAt(0).getRow(0).getCell(4).getStringCellValue == "raportti.hakemusOid")
-    assert(wb.getSheetAt(0).getRow(0).getCell(5).getStringCellValue == "raportti.oppilaitos")
+    assert(wb.getSheetAt(0).getRow(0).getCell(5).getStringCellValue == "Oppilaitos SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(6).getStringCellValue == "Toimipiste SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(7).getStringCellValue == "Hakukohde SV")
     assert(wb.getSheetAt(0).getRow(0).getCell(8).getStringCellValue == "raportti.hakukohdeOid")
@@ -2004,14 +2005,15 @@ class ExcelWriterSpec extends AnyFlatSpec {
       translations = translations,
       data = data,
       yksittaisetHakijat = 450,
-      naytaHakutoiveet = true
+      naytaHakutoiveet = true,
+      tulostustapa = "hakukohteittain"
     )
 
     assert(workbook.getNumberOfSheets == 1)
     val sheet = workbook.getSheetAt(0)
     // Otsikkorivi
     val headingRow = sheet.getRow(0)
-    assert(headingRow.getCell(0).getStringCellValue == "Otsikko SV")
+    assert(headingRow.getCell(0).getStringCellValue == "Hakukohde SV")
     assert(headingRow.getCell(1).getStringCellValue == "Hakijat SV")
     assert(headingRow.getCell(2).getStringCellValue == "Ensisijaisia SV")
     assert(headingRow.getCell(3).getStringCellValue == "Varasija SV")
@@ -2095,6 +2097,80 @@ class ExcelWriterSpec extends AnyFlatSpec {
 
   }
 
+  it should "return excel with correct tulostustapa heading" in {
+    val data = List(
+      HakeneetHyvaksytytVastaanottaneetResult(
+        otsikko = Map(
+          En -> "OSAO, Haukiputaan yksikkö",
+          Fi -> "OSAO, Haukiputaan yksikkö",
+          Sv -> "OSAO, Haukiputaan yksikkö"
+        ),
+        hakijat = 354,
+        ensisijaisia = 95,
+        varasija = 354,
+        hyvaksytyt = 100,
+        vastaanottaneet = 0,
+        lasna = 0,
+        poissa = 0,
+        ilmYht = 0,
+        aloituspaikat = 100,
+        toive1 = 95,
+        toive2 = 91,
+        toive3 = 76,
+        toive4 = 45,
+        toive5 = 25,
+        toive6 = 11,
+        toive7 = 11
+      ),
+    )
+
+    val workbook = ExcelWriter.writeHakeneetHyvaksytytVastaanottaneetRaportti(
+      asiointikieli = "sv",
+      translations = translations,
+      data = data,
+      yksittaisetHakijat = 300,
+      naytaHakutoiveet = false,
+      tulostustapa = "oppilaitoksittain"
+    )
+
+    assert(workbook.getNumberOfSheets == 1)
+    val sheet = workbook.getSheetAt(0)
+    // Otsikkorivi
+    val headingRow = sheet.getRow(0)
+    assert(headingRow.getCell(0).getStringCellValue == "Oppilaitos SV")
+    assert(headingRow.getCell(1).getStringCellValue == "Hakijat SV")
+    assert(headingRow.getCell(2).getStringCellValue == "Ensisijaisia SV")
+    assert(headingRow.getCell(3).getStringCellValue == "Varasija SV")
+    assert(headingRow.getCell(4).getStringCellValue == "Hyväksytyt SV")
+    assert(headingRow.getCell(5).getStringCellValue == "Vastaanottaneet SV")
+    assert(headingRow.getCell(6).getStringCellValue == "Läsnä SV")
+    assert(headingRow.getCell(7).getStringCellValue == "Poissa SV")
+    assert(headingRow.getCell(8).getStringCellValue == "IlmYht SV")
+    assert(headingRow.getCell(9).getStringCellValue == "Aloituspaikat SV")
+    assert(headingRow.getLastCellNum == 10)
+    assert(headingRow.getCell(10) == null)
+
+    // Datarivit
+    assert(sheet.getRow(1).getCell(0).getStringCellValue == "OSAO, Haukiputaan yksikkö")
+    assert(sheet.getRow(1).getCell(1).getStringCellValue == "354")
+    assert(sheet.getRow(1).getCell(2).getStringCellValue == "95")
+    assert(sheet.getRow(1).getCell(3).getStringCellValue == "354")
+    assert(sheet.getRow(1).getCell(4).getStringCellValue == "100")
+    assert(sheet.getRow(1).getCell(5).getStringCellValue == "0")
+    assert(sheet.getRow(1).getCell(6).getStringCellValue == "0")
+    assert(sheet.getRow(1).getCell(7).getStringCellValue == "0")
+    assert(sheet.getRow(1).getCell(8).getStringCellValue == "0")
+    assert(sheet.getRow(1).getCell(9).getStringCellValue == "100")
+    assert(sheet.getRow(1).getCell(10) == null)
+    // summarivit
+    assert(sheet.getRow(2).getCell(0).getStringCellValue == "Yhteensä SV")
+    assert(sheet.getRow(2).getCell(1).getStringCellValue == "354")
+    assert(sheet.getRow(3).getCell(0).getStringCellValue == "Yksittäiset hakijat SV")
+    assert(sheet.getRow(3).getCell(1).getStringCellValue == "300")
+    assert(sheet.getPhysicalNumberOfRows == 4)
+
+  }
+
   it should "return excel without Toive sarakkeet if naytaHakutoiveet is false" in {
     val data = List(
       HakeneetHyvaksytytVastaanottaneetResult(
@@ -2127,14 +2203,15 @@ class ExcelWriterSpec extends AnyFlatSpec {
       translations = translations,
       data = data,
       yksittaisetHakijat = 300,
-      naytaHakutoiveet = false
+      naytaHakutoiveet = false,
+      tulostustapa = "hakukohteittain"
     )
 
     assert(workbook.getNumberOfSheets == 1)
     val sheet = workbook.getSheetAt(0)
     // Otsikkorivi
     val headingRow = sheet.getRow(0)
-    assert(headingRow.getCell(0).getStringCellValue == "Otsikko SV")
+    assert(headingRow.getCell(0).getStringCellValue == "Hakukohde SV")
     assert(headingRow.getCell(1).getStringCellValue == "Hakijat SV")
     assert(headingRow.getCell(2).getStringCellValue == "Ensisijaisia SV")
     assert(headingRow.getCell(3).getStringCellValue == "Varasija SV")
