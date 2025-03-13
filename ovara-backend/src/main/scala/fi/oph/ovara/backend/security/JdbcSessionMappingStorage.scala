667d8ce116a5cf5884f5a87dd3bfac05f22022c6
Marja Kari <marja.kari@amban.fi>
2025-03-13 19:50:47 +0200

    kyselyt kuntoon

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/security/JdbcSessionMappingStorage.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/security/JdbcSessionMappingStorage.scala
@@ -11,12 +11,12 @@ class JdbcSessionMappingStorage(sessionRepository: SessionRepository[Session], s
 
   val LOG = LoggerFactory.getLogger(classOf[JdbcSessionMappingStorage])
 
-  val mappingTableName = schema+".virkailija_cas_client_session"
-  val sessionTableName = schema+".virkailija_session"
+  val mappingTableName = s"${schema}.virkailija_cas_client_session"
+  val sessionTableName = s"${schema}.virkailija_session"
   @Override
   def removeSessionByMappingId(mappingId: String): HttpSession = {
-    LOG.info("Poistetaan sessiomappaus cas tiketillä")
-    val query = sql"""SELECT virkailija_session_id FROM $mappingTableName WHERE mapped_ticket_id = $mappingId""".as[String].headOption
+    LOG.debug(s"Poistetaan sessiomappaus cas tiketillä $mappingId")
+    val query = sql"""SELECT virkailija_session_id FROM #$mappingTableName WHERE mapped_ticket_id = $mappingId""".as[String].headOption
     val sessionIdOpt = ovaraDatabase.run(query, "removeSessionByMappingId")
 
     sessionIdOpt
@@ -27,23 +27,22 @@ class JdbcSessionMappingStorage(sessionRepository: SessionRepository[Session], s
 
   @Override
   def removeBySessionById(sessionId: String): Unit = {
-    LOG.info("Poistetaan sessiomappaus session id:llä")
-    val sql = sqlu"""DELETE FROM $mappingTableName WHERE virkailija_session_id = $sessionId"""
+    LOG.debug(s"Poistetaan sessiomappaus session id:llä $sessionId")
+    val sql = sqlu"""DELETE FROM #$mappingTableName WHERE virkailija_session_id = $sessionId"""
     ovaraDatabase.run(sql, "removeBySessionById")
   }
 
   @Override
   def addSessionById(mappingId: String, session: HttpSession): Unit = {
-    LOG.info("Lisätään sessiomappaus")
-    LOG.info(s"mappingId: $mappingId, sessionId: ${session.getId}")
-    val sql = sqlu"""INSERT INTO $mappingTableName (mapped_ticket_id, virkailija_session_id) VALUES ($mappingId, ${session.getId}) ON CONFLICT (mapped_ticket_id) DO NOTHING"""
+    LOG.debug(s"Lisätään sessiomappaus, mappingId: $mappingId, sessionId: ${session.getId}")
+    val sql = sqlu"""INSERT INTO #$mappingTableName (mapped_ticket_id, virkailija_session_id) VALUES ($mappingId, ${session.getId}) ON CONFLICT (mapped_ticket_id) DO NOTHING"""
     ovaraDatabase.run(sql, "addSessionById")
   }
 
   @Override
   def clean(): Unit = {
-    LOG.info("Siivotaan sessiomappaukset joista ei löydy sessiota")
-    val sql = sqlu"""DELETE FROM $mappingTableName WHERE virkailija_session_id NOT IN (SELECT session_id FROM $sessionTableName)"""
+    LOG.debug("Siivotaan sessiomappaukset joille ei löydy sessiota")
+    val sql = sqlu"""DELETE FROM #$mappingTableName WHERE virkailija_session_id NOT IN (SELECT session_id FROM #$sessionTableName)"""
     ovaraDatabase.run(sql, "clean")
   }
 
