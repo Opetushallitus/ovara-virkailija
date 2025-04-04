0432c6b49236a7acecd6c51332be53e44165552a
Marja Kari <marja.kari@amban.fi>
2025-04-04 18:53:55 +0300

    OK-886 varmistus ettei hakuja fetchata ilman alkamiskautta

--- ovara-ui/src/app/hooks/useFetchHaut.ts
+++ ovara-ui/src/app/hooks/useFetchHaut.ts
@@ -1,5 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { doApiFetch } from '@/app/lib/ovara-backend/api';
+import { useCommonSearchParams } from '@/app/hooks/searchParams/useCommonSearchParams';
+import { isNullish } from 'remeda';
 
 const fetchHaut = (queryParamsStr: string | null) => {
   return doApiFetch('haut', {
@@ -11,9 +13,19 @@ export const useFetchHaut = (
   alkamiskaudetQueryParams: string | null,
   fetchEnabled: boolean,
 ) => {
+  const { selectedAlkamiskaudet } = useCommonSearchParams();
+  const hasSelectedAlkamiskaudet =
+    !isNullish(selectedAlkamiskaudet) && selectedAlkamiskaudet.length > 0;
+  // lisävarmistus koska muuten hakee pelkällä haun tyyppi -parametrilla
+  // ennenkuin valittu alkamiskausi on ehtinyt päivittyä urliin
+  const hasAlkamiskaudetInQuery = alkamiskaudetQueryParams
+    ?.split('&')
+    .some((param) => param.startsWith('alkamiskausi='));
+  const shouldFetch =
+    fetchEnabled && hasSelectedAlkamiskaudet && hasAlkamiskaudetInQuery;
   return useQuery({
     queryKey: ['fetchHaut', alkamiskaudetQueryParams],
     queryFn: () => fetchHaut(alkamiskaudetQueryParams),
-    enabled: fetchEnabled,
+    enabled: shouldFetch,
   });
 };
