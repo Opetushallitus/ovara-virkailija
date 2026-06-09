cca2ab8118e898eab0e5009076b3363f208619b3
August Kilponen <august.kilponen@gmail.com>
2026-06-09 13:10:36 +0300

    Lisätty testi ettei Valpas-rajapinnasta palaudu hakemuksia joiden oidin pituus ei ole 35 merkkiä

--- ovara-backend/src/test/scala/fi/oph/ovara/backend/valpas/ValpasServiceTest.scala
+++ ovara-backend/src/test/scala/fi/oph/ovara/backend/valpas/ValpasServiceTest.scala
@@ -83,6 +83,16 @@ class ValpasServiceTest extends AnyFlatSpec with Matchers with BeforeAndAfterEac
     assert(hakemus.oppijaOid == OPPIJANUMERO)
   }
 
+  it should "not return hakemus when hakemus_oid is not 35 characters" in {
+    initSchema()
+    insertHakemus(hakemusOid = "1.2.246.562.11.3511892")
+
+    val response = service.getValpasTiedot(List(OPPIJANUMERO), false)
+
+    assert(response.isRight)
+    assert(response.toOption.get.isEmpty)
+  }
+
   it should "return hakemus for active haku when vainAktiiviset is true" in {
     initSchema()
     insertHakemus()
