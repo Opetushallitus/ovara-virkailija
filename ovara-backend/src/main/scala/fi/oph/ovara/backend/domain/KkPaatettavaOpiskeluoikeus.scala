01050600013a4fe938c3f01886ea71bc77d99c32
Marja Kari <marja.kari@amban.fi>
2026-06-11 14:33:43 +0300

    OPHYOS-193 hakuparametrivälilehdelle raportin nimi ja tulostusaika

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/domain/KkPaatettavaOpiskeluoikeus.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/domain/KkPaatettavaOpiskeluoikeus.scala
@@ -1,6 +1,25 @@
 package fi.oph.ovara.backend.domain
 
-class KkPaatettavaOpiskeluoikeus {
-
-
-}
+case class KkPaatettavaOpiskeluoikeus(
+  oppijanumero: String,
+  henkilotunnus: Option[String],
+  syntymaaika: String,
+  sukunimi: String,
+  etunimet: String,
+  kutsumanimi: String,
+  opiskelijaAvain: String,
+  opiskeluoikeusAvain: String,
+  opiskeluoikeudenNimi: Kielistetty,
+  opiskeluoikeudenPaattymispvm: Option[String],
+  opiskeluoikeudenViimeisinTila: String,
+  hakemusOid: String,
+  hakuOid: String,
+  hakuNimi: Kielistetty,
+  hakukohdeOid: String,
+  hakukohdeNimi: Kielistetty,
+  oppilaitosOid: String,
+  oppilaitosNimi: Kielistetty,
+  vastaanottoAjankohta: String,
+  koulutusluokitusKoodi: String,
+  uudenOpiskeluoikeudenAlkamispvm: String
+)
