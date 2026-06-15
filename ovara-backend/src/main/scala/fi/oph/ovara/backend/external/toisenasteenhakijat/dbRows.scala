c3a0b58debd14b65caae6b7d2d151168830a90b3
Mikko Siukola <mikko.siukola@cybercom.com>
2026-06-15 16:28:26 +0300

    OPHSUPA-328 Add various fields from gen_hakemus

--- ovara-backend/src/main/scala/fi/oph/ovara/backend/external/toisenasteenhakijat/dbRows.scala
+++ ovara-backend/src/main/scala/fi/oph/ovara/backend/external/toisenasteenhakijat/dbRows.scala
@@ -1,5 +1,7 @@
 package fi.oph.ovara.backend.external.toisenasteenhakijat
 
+import java.time.OffsetDateTime
+
 case class HakijaRow(
   oppijanumero: String,
   hakemusOid: String,
@@ -8,7 +10,21 @@ case class HakijaRow(
   lahiosoite: String,
   postinumero: String,
   postitoimipaikka: String,
-  hakuOid: String
+  hakuOid: String,
+  etunimet: Option[String],
+  kutsumanimi: Option[String],
+  sukunimi: Option[String],
+  hetu: Option[String],
+  asuinmaa: Option[String],
+  kansalaisuudet: Seq[String],
+  kotikunta: Option[String],
+  sukupuoli: Option[Int],
+  koulutusmarkkinointilupa: Option[Boolean],
+  kiinnostunutOppisopimuksesta: Option[Boolean],
+  sahkoinenviestintalupa: Option[Boolean],
+  valintatuloksenJulkaisulupa: Option[Boolean],
+  jatetty: Option[OffsetDateTime],
+  muokattu: Option[OffsetDateTime]
 ) {
   def asHakija(hakutoiveet: Seq[HakijaHakutoive]): ToisenAsteenHakija =
     ToisenAsteenHakija(
@@ -18,9 +34,23 @@ case class HakijaRow(
       lahiosoite = lahiosoite,
       postinumero = postinumero,
       postitoimipaikka = postitoimipaikka,
+      hetu = hetu,
+      sukunimi = sukunimi,
+      etunimet = etunimet,
+      kutsumanimi = kutsumanimi,
+      maa = asuinmaa,
+      kansalaisuudet = kansalaisuudet,
+      kotikunta = kotikunta,
+      sukupuoli = sukupuoli.map(_.toString),
+      koulutusmarkkinointilupa = koulutusmarkkinointilupa,
+      kiinnostunutoppisopimuksesta = kiinnostunutOppisopimuksesta,
+      sahkoisenAsioinninLupa = sahkoinenviestintalupa,
       hakemus = HakijaHakemus(
         hakemusnumero = hakemusOid,
-        hakutoiveet = hakutoiveet
+        hakutoiveet = hakutoiveet,
+        hakemuksenJattopaiva = jatetty,
+        hakemuksenMuokkauspaiva = muokattu,
+        julkaisulupa = valintatuloksenJulkaisulupa
       )
     )
 }
